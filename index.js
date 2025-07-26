91955248fb141589c54bd6deda597f2bb940b87d
David Ogunfiditimi Oluwatayo <davidsome2004@gmail.com>
2025-07-26 21:43:04 +0100

    feat: Implement CORS middleware with allowed origins for enhanced security

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ dotenv.config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 
 connectDB();
-app.use(cors());
+const allowedOrigins = [
+    'https://ecommerce-frontend-eight-blue.vercel.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
+app.options('/', cors());
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
@@ -27,7 +43,7 @@ app.use('/api/admin', adminRoutes); // Or wherever appropriate
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;    
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
